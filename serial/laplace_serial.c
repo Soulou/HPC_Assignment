689f0834ba57ec3e33fa86c23df4080e4e13706f
Soulou <leo@unbekandt.eu>
2013-11-27 11:42:58 +0000

    Update from Jacobi to Gauss Seidel

--- serial/laplace_serial.c
+++ serial/laplace_serial.c
@@ -58,9 +58,9 @@ void print_data_matrix(double ** matrix, int n) {
 	FILE * results = fopen("laplace_results.txt", "w");
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < n; j++) {
-			fprintf(results, "%1.10lf %1.10lf %1.10lf\n", (double)i/(n-1.), (double)j/(n-1.), matrix[j][i]);
-			/* printf("%1.10lf\n", matrix[i][j]); */
+			fprintf(results, "%1.10lf %1.10lf %1.30lf\n", (double)i/(n-1.), (double)j/(n-1.), matrix[j][i]);
 		}
+		fprintf(results, "\n");
 	}
 	fclose(results);
 }
@@ -72,6 +72,16 @@ void print_matrix(double ** matrix, int n) {
 		}
 		printf("\n");
 	}
+}
+
+double ** copy_matrix(double ** m, int n) {
+	double ** res = (double **)malloc(sizeof(double *) * n);
+	for(int i = 0; i < n; i++) {
+		res[i] = (double *)malloc(sizeof(double) * n);
+		for(int j = 0; j < n ; j++)
+			res[i][j] = m[i][j];
+	}
+	return res;
 }
 
 void free_matrix(double ** m, int n) {
@@ -91,28 +101,23 @@ double ** diff_matrices(double ** m1, double ** m2, int n) {
 	return res;
 }
 
-double ** iterate(double ** matrix, int n) {
-	double ** matrix2 = init_matrix(n, 0.0);
+void iterate(double ** matrix, int n) {
 	for(int i = 1; i < n-1; i++) {
 		for(int j = 1; j < n-1; j++) {
-			matrix2[i][j] = (matrix[i-1][j] + matrix[i+1][j] + matrix[i][j-1] + matrix[i][j+1]) * 0.25;
+			matrix[i][j] = (matrix[i-1][j] + matrix[i+1][j] + matrix[i][j-1] + matrix[i][j+1]) * 0.25;
 		}
 	}
-	return matrix2;
 }
 
-double error(double ** old, double ** new, int n) {
-	double ** diff = diff_matrices(new, old, n);
+double error(double ** old_m, double ** new_m, int n) {
+	double ** diff = diff_matrices(new_m, old_m, n);
 	double res = NORM(diff, n);
 	free_matrix(diff, n);
 	return res;
 }
 
-double convergence_rate(double ** oldold, double ** old, double ** new, int n) {
-	/* printf("NORM(new): %0.10lf\n", NORM(new, n)); */
-	/* printf("NORM(old): %0.10lf\n", NORM(old, n)); */
-	/* printf("NORM(oldold): %0.10lf\n", NORM(oldold, n)); */
-	return error(old, new, n) / error(oldold, old, n);
+double convergence_rate(double ** old_mold_m, double ** old_m, double ** new_m, int n) {
+	return error(old_m, new_m, n) / error(old_mold_m, old_m, n);
 }
 
 int main(int argc, char * argv[])
@@ -136,15 +141,19 @@ int main(int argc, char * argv[])
 	int nb_iterations = 0;
 	do {
 		nb_iterations++;
+
 		prev_prev_m = prev_m;
-		prev_m = new_m;
-		new_m = iterate(prev_m, n);
+		prev_m = copy_matrix(new_m, n);
+		iterate(new_m, n);
 	  it_error = error(prev_m, new_m, n);
-		fprintf(f_error, "%d %lf\n", nb_iterations, it_error);
-		if(nb_iterations > 2)
-			fprintf(f_convergence_rate, "%d %lf\n", nb_iterations, convergence_rate(prev_prev_m, prev_m, new_m, n));
-	} while(it_error > e);
-	// print_matrix(new_m, n);
+		fprintf(f_error, "%d %1.30lf\n", nb_iterations, it_error);
+		if(nb_iterations % 30 == 0 && it_error < e)
+			break;
+		if(nb_iterations > 2) {
+			fprintf(f_convergence_rate, "%d %1.30lf\n", nb_iterations, convergence_rate(prev_prev_m, prev_m, new_m, n));
+			free_matrix(prev_prev_m, n);
+		}
+	} while(1);
 	printf("Norm(m) = %lf\n", NORM(new_m, n));
 	print_data_matrix(new_m, n);
 	fprintf(stderr, "Number of iterations: %d\n", nb_iterations);
